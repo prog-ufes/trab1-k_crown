d2f89bcc3aa2c7619df59be08faec6add5dfb3c7
Philipe Aguiar Marques <34419519+xRiku@users.noreply.github.com>
2018-11-19 10:12:41 -0200

    Melhor explicado, parcialmente igual Iris

--- teste.c
+++ teste.c
@@ -4,13 +4,14 @@
 #include <string.h>
 
 int main(){
-    FILE *file = fopen("teste1.txt", "r");
-    char *str = malloc(sizeof(char)*1000);
+    FILE *file = fopen("teste1.txt", "r"), *file2 = fopen("teste2.txt", "r");
+    char *str = malloc(sizeof(char)*1000), *str2 = malloc(sizeof(char)*1000);
     if (file == NULL) {
     printf ("Erro na abertura de arquivo! Programa terminado...");
     exit (1);
     }
-    int n = 0, linhas = 0, i, quebra = 0, j = 0;
+    int n = 0, linhas = 0, linhas2 = 0, i, quebra = 0, j = 0;
+    // Calculo do numero de linhas
     while(!feof(file)){
         //fscanf(file, "%[^\n]", str);
         fgets(str, 1000, file);
@@ -24,11 +25,24 @@ int main(){
         }
         linhas++;
     }
+    while(!feof(file2)){
+        fgets(str2, 1000, file2);
+        linhas2++;
+    }
+    //retomar o arquivo para o inicio
+    rewind(file2);
     rewind(file);
+    //alocando memoria para a matriz de floats do arq 1
     float **vetor = malloc(sizeof(float*) * linhas);
     for (i = 0; i < linhas; i++){
         vetor[i] = malloc(sizeof(float)*(n+1));
     }
+    //alocando memoria para a matriz de floats do arq 2
+    float **vetor2 = malloc(sizeof(float*) * linhas2);
+    for (i = 0; i < linhas2; i++){
+        vetor2[i] = malloc(sizeof(float)*n);
+    }
+    //Quebrando a string em varias para converter o valores 1
     for (i = 0; i < linhas; i++){
         //fscanf(file, "%[^\n]", str);
         fgets(str, 1000, file);
@@ -41,15 +55,41 @@ int main(){
         }
         j = 0;
     }
+    //Quebrando a string em varias para converter o valores 2
+    for (i = 0; i < linhas2; i++){
+        //fscanf(file, "%[^\n]", str);
+        fgets(str2, 1000, file2);
+        char *strnew2 = NULL;
+        strnew2 = strtok(str2, ",\n");
+        while(strnew2 != NULL){
+            vetor2[i][j] = atof(strnew2);
+            strnew2 = strtok(NULL, ",\n");
+            j++;
+        }
+        j = 0;
+    }
+
+    //Printando a matriz 1
     for(i = 0; i < linhas; i++){
         for (j = 0; j < n+1; j++)
             printf("%.2f ", vetor[i][j]);
         putchar('\n');
         free(vetor[i]);
     }
+    puts("*****************************************");
+    //Printando a matriz 2
+    for(i = 0; i < linhas2; i++){
+        for (j = 0; j < n; j++)
+            printf("%.2f ", vetor2[i][j]);
+        putchar('\n');
+        free(vetor2[i]);
+    }
     free(str);
+    free(str2);
     free(vetor);
+    free(vetor2);
     fclose(file);
+    fclose(file2);
     return 0;
 
 }
